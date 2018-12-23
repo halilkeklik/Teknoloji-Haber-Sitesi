fe1cc81f2e775cf08175a94894d8a8da3565993e
meamert <bymali32@gmail.com>
2018-12-23 15:31:12 +0300

    Home Controller eklendi.

--- WebOdevi/Controllers/HomeController.cs
+++ WebOdevi/Controllers/HomeController.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebOdevi.Models;
 
 namespace WebOdevi.Controllers
 {
+
     public class HomeController : Controller
     {
         // GET: Home
+        webodevDB db = new webodevDB();
         public ActionResult Index()
         {
-            return View();
+            var post = db.Post.OrderByDescending(p => p.PostId).ToList();
+            return View(post);
         }
     }
 }
