7e3a3b7269c2bbea0b28a1201f2281554c45b897
Üsame Tarık Temel <utariktemel@gmail.com>
2018-12-24 16:44:01 +0300

    Globalization Eklendi. Paketler güncellendi.

--- WebOdevi/Global.asax.cs
+++ WebOdevi/Global.asax.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Threading;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
@@ -13,6 +15,22 @@ namespace WebOdevi
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+
+        protected void Application_AcquireRequestState(object sender, EventArgs e)
+        {
+            if (HttpContext.Current.Session == null) return;
+
+            var cultureInfo = (CultureInfo)Session["Culture"];
+            if (cultureInfo == null)
+            {
+                var languageName = "tr";
+                cultureInfo = new CultureInfo(languageName);
+                Session["Culture"] = cultureInfo;
+            }
+
+            Thread.CurrentThread.CurrentUICulture = cultureInfo;
+            Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture(cultureInfo.Name);
         }
     }
 }
