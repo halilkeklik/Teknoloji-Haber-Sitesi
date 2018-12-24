735e2dad90ff03da92f500a079f8f1edf7e004ca
meamert <bymali32@gmail.com>
2018-12-24 15:37:28 +0300

    Kategori Listeleme eklendi, PagedList eklendi,  admin panelinde küçük değişiklikler yapıldı, widgetler eklendi, arama özelliği eklendi, Ajax yorum yapma özelliği kaldırıldı, normal yorum özelliği eklendi ve arayüzde küçük değişiklikler.

--- WebOdevi/Controllers/HomeController.cs
+++ WebOdevi/Controllers/HomeController.cs
@@ -4,7 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using WebOdevi.Models;
-
+using PagedList;
+using PagedList.Mvc;
 namespace WebOdevi.Controllers
 {
 
@@ -12,29 +13,85 @@ namespace WebOdevi.Controllers
     {
         // GET: Home
         webodevDB db = new webodevDB();
-        public ActionResult Index()
+        public ActionResult Index(int Page = 1)
         {
-            var post = db.Post.OrderByDescending(p => p.PostId).ToList();
+
+            var post = db.Post.OrderByDescending(p => p.PostId).ToPagedList(Page, 3);
             return View(post);
+        }
+
+        public ActionResult Category(int id, int Page = 1)
+        {
+            var posts = db.Post.Where(p => p.CatId == id).OrderByDescending(i => i.PostId).ToPagedList(Page, 3);
+            var post = db.Cat.Where(c => c.CatId == id).SingleOrDefault();
+            if (post == null)
+            {
+                return HttpNotFound();
+            }
+            return View(posts);
         }
 
         public ActionResult Post(int id)
         {
             var post = db.Post.Where(p => p.PostId == id).SingleOrDefault();
-            if(post==null)
+            if (post == null)
             {
                 return HttpNotFound();
             }
             return View(post);
         }
 
-        public JsonResult Comment(string comment,int postids, int yazdimi)
-        {
-                var userid = Session["userid"];
-                db.Comment.Add(new Comment { UserId = Convert.ToInt32(Session["userid"]), PostId = postids, CommentContent = comment, CommentDate = DateTime.Now });
-                db.SaveChanges();
+        //public JsonResult Comment(string comment, int postids)
+        //{
+        //    var userid = Session["userid"];
+        //    Comment comments = new Comment
+        //    {
+        //        UserId = Convert.ToInt32(Session["userid"]),
+        //        PostId = postids,
+        //        CommentContent = comment,
+        //        CommentDate = DateTime.Now
+        //    };
+        //    db.Comment.Add(comments);
+        //    db.SaveChanges();
 
-                return Json(true, JsonRequestBehavior.AllowGet);
+        //    return Json(comments);
+        //}
+
+        public ActionResult CreateComment(int id)
+        {
+            var varmi = db.Post.Where(p => p.PostId == id).SingleOrDefault();
+            if (varmi == null)
+            {
+                return HttpNotFound();
+            }
+            Session["postid"] = id;
+            return View();
         }
+
+        [HttpPost]
+        public ActionResult CreateComment(Comment comment)
+        {
+            comment.CommentDate = DateTime.Now;
+            comment.UserId =Convert.ToInt32(Session["userid"]);
+            comment.PostId = Convert.ToInt32(Session["postid"]);
+            db.Comment.Add(comment);
+            db.SaveChanges();
+            Session["postid"] = "";
+            ModelState.Clear();
+            return View();
+        }
+
+
+        public ActionResult CatWidget()
+        {
+            return View(db.Cat.ToList());
+        }
+
+        public ActionResult Search(string search = null, int Page = 1)
+        {
+            var searchend = db.Post.Where(p => p.Context.Contains(search)).OrderByDescending(i=>i.PostId).ToPagedList(Page, 3);
+            return View(searchend);
+        }
+
     }
 }
